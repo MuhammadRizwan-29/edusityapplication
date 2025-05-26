8444a9c5d21cc244b0e5f7025829e46847366855
MuhammadRizwan-29 <engrrizwanjamali@gmail.com>
2025-05-26 11:55:12 +0500

    Header & Hero content updated

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,24 @@
 import "./Navbar.css";
+import logo from "../../assets/logo.png";
 
 export default function Navbar() {
   return (
-    <header>
-      <div></div>
+    <header className="px-10">
+      <div className="logo">
+        <img src={logo} alt="logo-file" />
+      </div>
+      <div className="menu">
+        <ul>
+          <li>Home</li>
+          <li>Program</li>
+          <li>About us</li>
+          <li>Campus</li>
+          <li>Testimonials</li>
+          <li>
+            <button>Contact Us</button>
+          </li>
+        </ul>
+      </div>
     </header>
   );
 }
